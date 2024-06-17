9664ee1a5f946726c6e8785a752ca9936ce85bca
Kathir-23 <kathirkrish100@gmail.com>
2024-06-17 17:14:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -15,31 +15,31 @@ for (i = 0; i<document.querySelectorAll(".drum").length; i++){
 function keyPress(textt) {  
     switch (textt) {
         case "w": 
-         var audioOne = new Audio("sounds/tom-1.mp3");
+         var audioOne = new Audio("https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/tom-1.mp3");
                audioOne.play(); 
         break;
         case "a": 
-         var audioTwo = new Audio('sounds/tom-2.mp3');
+         var audioTwo = new Audio('https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/tom-2.mp3');
                audioTwo.play(); 
         break;
         case "s":
-            var audioThree = new Audio('sounds/tom-3.mp3');
+            var audioThree = new Audio('https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/tom-3.mp3');
                audioThree.play(); 
         break;
         case "d":
-            var audioFour = new Audio('sounds/tom-4.mp3');
+            var audioFour = new Audio('https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/tom-4.mp3');
                audioFour.play(); 
         break;
         case "j":
-            var audioFive = new Audio('sounds/snare.mp3');
+            var audioFive = new Audio('https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/snare.mp3');
                audioFive.play(); 
         break;
         case "k":
-            var audioSix = new Audio('sounds/kick-bass.mp3');
+            var audioSix = new Audio('https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/kick-bass.mp3');
                audioSix.play(); 
         break;
         case "l":
-            var audioSeven = new Audio('sounds/crash.mp3');
+            var audioSeven = new Audio('https://github.com/Kathir-23/PROJECT-DRUMS/blob/main/crash.mp3');
                audioSeven.play(); 
         break;
         default:
